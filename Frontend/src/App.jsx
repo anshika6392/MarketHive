93c472d7dae005efb646383bac39ca291a52941b
anshika6392 <prachi639267@gmail.com>
2026-08-15 19:22:14 +0530

    Added new seller and admin features

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -11,6 +11,10 @@ import BuyNow from "./pages/User/BuyNow";
 import OrderDetails from "./pages/User/OrderDetails";
 import SellerLogin from "./pages/seller/SellerLogin";
 import Checkout from "./pages/User/Checkout";
+import AdminDashboard from './pages/admin/AdminDashboard';
+import ManageSellers from './pages/admin/ManageSellers';
+import SellerDashboard from './pages/seller/SellerDashboard';
+import SellerProducts from './pages/seller/SellerProducts';
 
 const App = () => {
   return (
@@ -27,7 +31,13 @@ const App = () => {
       <Route path='/buyNow/:productId' element={<BuyNow></BuyNow>} />
       <Route path='/order' element={<OrderDetails></OrderDetails>} />
       <Route path='/checkout' element={<Checkout />} />
+
+
       <Route path='/seller/login' element={<SellerLogin></SellerLogin >} />
+      <Route path='/admin/dashboard' element={<AdminDashboard></AdminDashboard>} />
+      <Route path='/admin/manageSellers' element={<ManageSellers></ManageSellers>} />
+      <Route path='/seller/dashboard' element={<SellerDashboard></SellerDashboard>} />
+      <Route path='/seller/products' element={<SellerProducts></SellerProducts>} />
 
 
     </Routes>
